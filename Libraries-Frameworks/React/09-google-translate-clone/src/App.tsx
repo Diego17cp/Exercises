f9fd9575ccc29d9aee81f9db19fe468afe6d412a
Diego17 <diegoalbertocastropastor@gmail.com>
2025-01-21 12:19:22 -0500

    feat: initialize React project with TypeScript and Vite setup

---
+++ Libraries-Frameworks/React/09-google-translate-clone/src/App.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/react-in-jsx-scope */
+// import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Google Translate</h1>
+    </>
+  )
+}
+
+export default App
