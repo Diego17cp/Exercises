770cfac24920f6f9d523f26d9dc9da9e00640a0c
Diego17 <diegoalbertocastropastor@gmail.com>
2025-01-21 00:07:15 -0500

    feat: add index view for students with navigation options

---
+++ PHP/Simple Crud App/Views/students/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div{
+            display: flex;
+            gap: 20px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Simple CRUD App with PHP</h1>
+    <p>Select an option to do</p>
+    <div>
+        <a href="?action=read">View all students</a>
+        <a href="?action=createForm">Register new student</a>
+    </div>
+</body>
+</html>
