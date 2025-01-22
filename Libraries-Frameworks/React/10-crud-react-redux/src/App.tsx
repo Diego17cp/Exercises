0b1e9d6d893d0d9a590c9fe732338b1d681957cf
Diego17 <diegoalbertocastropastor@gmail.com>
2025-01-22 13:16:40 -0500

    feat: List of Users component for show the users data

--- Libraries-Frameworks/React/10-crud-react-redux/src/App.tsx
+++ Libraries-Frameworks/React/10-crud-react-redux/src/App.tsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react'
 import './App.css'
+import { ListOfUsers } from './components/ListOfUsers'
 
 function App() {
 
   return (
     <>
     <h1>Hello React</h1>
+    <ListOfUsers></ListOfUsers>
     </>
   )
 }
