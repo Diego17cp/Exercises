1bff1562faabb68971bef68eb88ec670b39166a5
Diego17 <diegoalbertocastropastor@gmail.com>
2025-01-25 13:47:49 -0500

    feat: add operations slice for calculator state management

---
+++ Libraries-Frameworks/FCC-Projects/05-calculator/src/store/operations/operationsSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+
+
+type OperationsState = number;
+
+const initialState: OperationsState = 0; 
+
+export const operationsSlice = createSlice({
+    name: 'operations',
+    initialState,
+    reducers: {
+        add: (state, action: PayloadAction<OperationsState>) => {
+            return state + action.payload
+        },
+        subtract: (state, action: PayloadAction<OperationsState>) => {
+            return state - action.payload
+        },
+        multiply: (state, action: PayloadAction<OperationsState>) => {
+            return state * action.payload
+        },
+        divide: (state, action: PayloadAction<OperationsState>) => {
+            return state / action.payload
+        },
+        clear: () => {
+            return initialState
+        }
+    }
+})
+
+export default operationsSlice.reducer;
