b4d6ed8d6a319ca496f9e717a58a6f0c73dd38ab
Diego17 <diegoalbertocastropastor@gmail.com>
2025-01-25 17:13:30 -0500

    fix: update state selector in Calculator component to use currentVal

--- Libraries-Frameworks/FCC-Projects/05-calculator/src/components/Calculator.tsx
+++ Libraries-Frameworks/FCC-Projects/05-calculator/src/components/Calculator.tsx
@@ -2,7 +2,7 @@ import { useAppSelector } from "../hooks/operations";
 import { Pad } from "./Pad";
 
 export const Calculator = () => {
-	const value = useAppSelector((state) => state.operations);
+	const { currentVal } = useAppSelector((state) => state.operations);
 
 	return (
 		<main className="mx-auto my-30 p-2 bg-cyan-800 rounded-lg shadow-lg h-120 w-85 justify-center items-center flex flex-col gap-2">
@@ -14,7 +14,7 @@ export const Calculator = () => {
 			>
 				<div className="width-full h-1/3 text-amber-400 flex justify-end items-start text-md"></div>
 				<div className="w-full h-2/3 flex justify-end items-center text-3xl text-white">
-					{value}
+					{currentVal}
 				</div>
 			</div>
 			<Pad></Pad>
