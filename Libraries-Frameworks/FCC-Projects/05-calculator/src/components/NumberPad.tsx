ccd87e6591df2833e0a30e436761a99a0263a98f
Diego17 <diegoalbertocastropastor@gmail.com>
2025-01-25 16:06:07 -0500

    feat: enhance NumberPad layout and add Pad component for calculator interface

--- Libraries-Frameworks/FCC-Projects/05-calculator/src/components/NumberPad.tsx
+++ Libraries-Frameworks/FCC-Projects/05-calculator/src/components/NumberPad.tsx
@@ -1,23 +1,24 @@
 
 
 export const NumberPad = () => {
-    const btnStyle = "bg-gray-400 cursor-pointer border-2 border-gray-300 text-xl active:bg-gray-500 active:scale-90 transition-all duration-500";
+    const btnStyle = "bg-gray-400 cursor-pointer text-xl active:bg-gray-500 active:scale-90 transition-all duration-500";
+    const border = {
+        border: "1px solid #d1d5dc"
+    }
     return (
-        <section className="h-52 w-52 bg-gray-800">
-            <div className="number-pad grid grid-cols-3 grid-rows-3 h-full">
-                <button className={btnStyle}>7</button>
-                <button className={btnStyle}>8</button>
-                <button className={btnStyle}>9</button>
-                <button className={btnStyle}>6</button>
-                <button className={btnStyle}>5</button>
-                <button className={btnStyle}>4</button>
-                <button className={btnStyle}>3</button>
-                <button className={btnStyle}>2</button>
-                <button className={btnStyle}>1</button>
-            </div>
-            <div className="flex w-full h-1/5">
-                <button className={`w-2/3 ${btnStyle}`}>0</button>
-                <button className={`w-1/3 ${btnStyle}`}>.</button>
+        <section className="h-full w-3/4 bg-gray-800">
+            <div className="number-pad grid grid-cols-3 grid-rows-4 h-full">
+                <button className={btnStyle} style={border}>7</button>
+                <button className={btnStyle} style={border}>8</button>
+                <button className={btnStyle} style={border}>9</button>
+                <button className={btnStyle} style={border}>6</button>
+                <button className={btnStyle} style={border}>5</button>
+                <button className={btnStyle} style={border}>4</button>
+                <button className={btnStyle} style={border}>3</button>
+                <button className={btnStyle} style={border}>2</button>
+                <button className={btnStyle} style={border}>1</button>
+                <button className={`col-span-2 ${btnStyle}`} style={border}>0</button>
+                <button className={`${btnStyle}`} style={border}>.</button>
             </div>
         </section>
     )
