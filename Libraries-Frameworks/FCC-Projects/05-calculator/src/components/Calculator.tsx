3ef91609327ed020c4e079b696526c3393ac99ce
Diego17 <diegoalbertocastropastor@gmail.com>
2025-01-25 18:12:18 -0500

    feat: add operationDisplay to manage and display current operations in Calculator

--- Libraries-Frameworks/FCC-Projects/05-calculator/src/components/Calculator.tsx
+++ Libraries-Frameworks/FCC-Projects/05-calculator/src/components/Calculator.tsx
@@ -2,7 +2,7 @@ import { useAppSelector } from "../hooks/operations";
 import { Pad } from "./Pad";
 
 export const Calculator = () => {
-	const { currentVal } = useAppSelector((state) => state.operations);
+	const { currentVal, operationDisplay } = useAppSelector((state) => state.operations);
 
 	return (
 		<main className="mx-auto my-30 p-2 bg-black border-2 border-gray-600 rounded-lg shadow-lg h-120 w-85 justify-center items-center flex flex-col gap-2">
@@ -12,7 +12,9 @@ export const Calculator = () => {
 					fontFamily: "Zen Dots, serif",
 				}}
 			>
-				<div className="width-full h-1/3 text-amber-400 flex justify-end items-start text-md"></div>
+				<div className="width-full h-1/3 text-amber-400 flex justify-end items-start text-md">
+					{operationDisplay}
+				</div>
 				<div className="w-full h-2/3 flex justify-end items-center text-3xl text-white">
 					{currentVal}
 				</div>
--- Libraries-Frameworks/FCC-Projects/05-calculator/src/store/operations/operationsSlice.ts
+++ Libraries-Frameworks/FCC-Projects/05-calculator/src/store/operations/operationsSlice.ts
@@ -1,85 +1,113 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
-
-export type Operations = 'add' | 'subtract' | 'multiply' | 'divide' | null
+export type Operations = "add" | "subtract" | "multiply" | "divide" | null;
 interface OperationsState {
-    currentVal: string,
-    operation: Operations,
-    previousVal: null | number
-} 
+	currentVal: string;
+	operation: Operations;
+	previousVal: null | number;
+	operationDisplay: string;
+}
 
-const initialState: OperationsState={
-    currentVal: "0",
-    operation: null,
-    previousVal: null
-}; 
+const initialState: OperationsState = {
+	currentVal: "0",
+	operation: null,
+	previousVal: null,
+	operationDisplay: "",
+};
 
 export const operationsSlice = createSlice({
-    name: 'operations',
-    initialState,
-    reducers: {
-        inputNumber: (state, action: PayloadAction<string>) => {
-            if(action.payload === "."){
-                if(state.currentVal.includes(".")) return
-                if(state.currentVal === "0"){
-                    state.currentVal = "0."
-                    return
-                }
-                state.currentVal += "."
-                return
-            }
-            if(state.currentVal==="0"){
-                state.currentVal = action.payload
-            } else{
-                state.currentVal += action.payload
-            }
-        },
-        selectOperation: (state, action: PayloadAction<Operations>) => {
-            state.previousVal = parseFloat(state.currentVal)
-            state.operation = action.payload
-            state.currentVal = "0"
-        },
-        calculate: (state) => {
-            if(state.previousVal === null || state.currentVal!== "0") return
-            if(state.previousVal === null || state.operation === null) {
-                state.currentVal = "NaN"
-                return
-            }
-            const currentVal = parseFloat(state.currentVal)
-            switch(state.operation){
-                case 'add':
-                    state.currentVal = (state.previousVal + currentVal).toString()
-                    break
-                case 'subtract':
-                    state.currentVal = (state.previousVal - currentVal).toString()
-                    break
-                case 'multiply':
-                    state.currentVal = (state.previousVal * currentVal).toString()
-                    break
-                case 'divide':
-                    if(state.currentVal !== "0"){
-                        state.currentVal = (state.previousVal / currentVal).toString()
-                    } else{
-                        state.currentVal = "NaN"
-                    }
-                    break
-            }
-            state.previousVal = null
-            state.operation = null
-        },
-        clear: (state) => {
-            state.currentVal = "0"
-            state.operation = null
-            state.previousVal = null
-        }
-    }
-})
+	name: "operations",
+	initialState,
+	reducers: {
+		inputNumber: (state, action: PayloadAction<string>) => {
+			if (action.payload === ".") {
+				if (state.currentVal.includes(".")) return;
+				if (state.currentVal === "0") {
+					state.currentVal = "0.";
+					state.operationDisplay = state.currentVal;
+					return;
+				}
+				state.currentVal += ".";
+				state.operationDisplay = state.currentVal;
+				return;
+			}
+			if (state.currentVal === "0") {
+				state.currentVal = action.payload;
+				state.operationDisplay = state.currentVal;
+			} else {
+				state.currentVal += action.payload;
+				state.operationDisplay = state.currentVal;
+			}
+		},
+		selectOperation: (state, action: PayloadAction<Operations>) => {
+			state.previousVal = parseFloat(state.currentVal);
+			state.operation = action.payload;
+			state.operationDisplay = `${state.currentVal} ${
+				action.payload === "add"
+					? "+"
+					: action.payload === "subtract"
+					? "-"
+					: action.payload === "multiply"
+					? "*"
+					: "/"
+			}`;
+			state.currentVal = "0";
+		},
+		calculate: (state) => {
+			if (state.operation === null) {
+				state.operationDisplay = `${state.currentVal} = ${state.currentVal}`;
+				return;
+			}
+			if (state.previousVal === null) {
+				state.currentVal = "NaN";
+				state.operationDisplay = "NaN";
+				return;
+			}
+			const currentVal = parseFloat(state.currentVal);
+			switch (state.operation) {
+				case "add":
+					state.currentVal = (
+						state.previousVal + currentVal
+					).toString();
+					state.operationDisplay = `${state.previousVal} + ${currentVal} = ${state.currentVal}`;
+					break;
+				case "subtract":
+					state.currentVal = (
+						state.previousVal - currentVal
+					).toString();
+					state.operationDisplay = `${state.previousVal} - ${currentVal} = ${state.currentVal}`;
+					break;
+				case "multiply":
+					state.currentVal = (
+						state.previousVal * currentVal
+					).toString();
+					state.operationDisplay = `${state.previousVal} * ${currentVal} = ${state.currentVal}`;
+					break;
+				case "divide":
+					if (currentVal !== 0) {
+						state.currentVal = (
+							state.previousVal / currentVal
+						).toString();
+						state.operationDisplay = `${state.previousVal} / ${currentVal} = ${state.currentVal}`;
+					} else {
+						state.currentVal = "NaN";
+						state.operationDisplay = "Cannot divide by 0";
+					}
+					break;
+			}
+			state.previousVal = null;
+			state.operation = null;
+		},
+		clear: (state) => {
+			state.currentVal = "0";
+			state.operation = null;
+			state.previousVal = null;
+			state.operationDisplay = "";
+		},
+	},
+});
 
 export default operationsSlice.reducer;
 
-export const {
-    inputNumber, 
-    selectOperation,
-    calculate,
-    clear
-} = operationsSlice.actions;
+export const { inputNumber, selectOperation, calculate, clear } =
+	operationsSlice.actions;
