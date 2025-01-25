c0564bd516632addcee0c98be440b8378ddde6cc
Diego17 <diegoalbertocastropastor@gmail.com>
2025-01-25 15:02:37 -0500

    fix: remove redundant height specification in NumberPad component

--- Libraries-Frameworks/FCC-Projects/05-calculator/src/components/NumberPad.tsx
+++ Libraries-Frameworks/FCC-Projects/05-calculator/src/components/NumberPad.tsx
@@ -4,7 +4,7 @@ export const NumberPad = () => {
     const btnStyle = "bg-gray-400 cursor-pointer border-2 border-gray-300 text-xl active:bg-gray-500 active:scale-90 transition-all duration-500";
     return (
         <section className="h-52 w-52 bg-gray-800">
-            <div className="number-pad grid grid-cols-3 grid-rows-3 h-full h-3/4">
+            <div className="number-pad grid grid-cols-3 grid-rows-3 h-full">
                 <button className={btnStyle}>7</button>
                 <button className={btnStyle}>8</button>
                 <button className={btnStyle}>9</button>
