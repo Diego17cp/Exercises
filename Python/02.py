f90dfccc7fa3de814dd4a85f27a3fd96be2546e7
Diego17 <diegoalbertocastropastor@gmail.com>
2025-01-27 21:26:41 -0500

    feat: exercise 3 solved

--- Python/02.py
+++ Python/02.py
@@ -1,3 +1,5 @@
+# Conditionals:
+
 # Ejercicio 1: Determinar el mayor de dos números
 # Pide al usuario que introduzca dos números y muestra un mensaje
 # indicando cuál es mayor o si son iguales
@@ -17,28 +19,35 @@
 # Pide al usuario dos números y una operación (+, -, *, /)
 # Realiza la operación y muestra el resultado (maneja la división entre zero)
 
-print("\nLet's do an simple operation \nIntroduce two numbers and an operation(+, -, *, /)")
-x = input("Introduce the first number: ")
-y = input("Introduce the second number: ")
-z = input("Introduce the operation: ")
+# print("\nLet's do an simple operation \nIntroduce two numbers and an operation(+, -, *, /)")
+# x = input("Introduce the first number: ")
+# y = input("Introduce the second number: ")
+# z = input("Introduce the operation: ")
 
-if z == "+":
-    print("\nThe result is: ", float(x) + float(y))
-elif z == "-":
-    print("\nThe result is: ", float(x) - float(y))
-elif z == "*":
-    print("\nThe result is: ", float(x) * float(y))
-elif z == "/":
-    if y == "0":
-        print("\nError: Yiu can't divide by zero")
-    else:
-        print("\nThe result is: ", float(x) / float(y))
-else:
-    print("\nError: Invalid operation")
+# if z == "+":
+#     print("\nThe result is: ", float(x) + float(y))
+# elif z == "-":
+#     print("\nThe result is: ", float(x) - float(y))
+# elif z == "*":
+#     print("\nThe result is: ", float(x) * float(y))
+# elif z == "/":
+#     if y == "0":
+#         print("\nError: Yiu can't divide by zero")
+#     else:
+#         print("\nThe result is: ", float(x) / float(y))
+# else:
+#     print("\nError: Invalid operation")
 
 # Ejercicio 3: Año bisiesto
 # Pide al usuario que introduzca un año y determina si es bisiesto.
 # Un año es bisiesto si es divisible por 4, excepto si es divisible por 100 pero no por 400.
+
+print("\nLet's determine if a year is leap year")
+year = input("Introduce the year: ")
+if float(year) % 4 == 0 and (float(year) % 100 != 0 or float(year) % 400 == 0):
+    print("\nThe year is leap year")
+else:
+    print("\nThe year is not leap year")
 
 # Ejercicio 4: Categorizar edades
 # Pide al usuario que introduzca una edad y la clasifique en:
