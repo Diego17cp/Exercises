614a69f5acbec8f009f77917dba19b3df4475ed2
Diego17 <diegoalbertocastropastor@gmail.com>
2025-02-01 20:05:18 -0500

    feat: transform search max exercise in a func

--- Python/08_funcs.py
+++ Python/08_funcs.py
@@ -13,4 +13,9 @@ def search_avg(numbers):
         tot += n
     return tot/len(numbers)
 
-print(f'The avg is {search_avg(numbers=[10, 20, 30, 40, 50])}')
+print(f'\nThe avg is {search_avg(numbers=[10, 20, 30, 40, 50])}\n')
+
+def search_max(numbers):
+    return max(numbers)
+
+print(f'The max number is: {search_max(numbers=[1, 2, 3, 4, 5])}\n')
