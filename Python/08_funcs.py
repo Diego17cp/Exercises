7571c92929ab89187974ccee8e0af12f439096c5
Diego17 <diegoalbertocastropastor@gmail.com>
2025-02-01 20:03:21 -0500

    feat: transform search average exercise in a func

--- Python/08_funcs.py
+++ Python/08_funcs.py
@@ -6,3 +6,11 @@ def get_mul_table(n=1):
         print(f'{n} * {i} = {n*i}')
 
 get_mul_table(5)
+
+def search_avg(numbers):
+    tot = 0
+    for n in numbers:
+        tot += n
+    return tot/len(numbers)
+
+print(f'The avg is {search_avg(numbers=[10, 20, 30, 40, 50])}')
