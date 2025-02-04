646929f4f8c58892666203a6025eb8703306aa40
Diego17 <diegoalbertocastropastor@gmail.com>
2025-02-04 16:00:24 -0500

    feat: add country filter functionality in App component

--- Libraries-Frameworks/React/11-technical-interview-list/src/App.tsx
+++ Libraries-Frameworks/React/11-technical-interview-list/src/App.tsx
@@ -7,8 +7,8 @@ function App() {
 	const [users, setUsers] = useState<User[]>([]);
 	const [showColors, setShowColors] = useState(false);
 	const [sortByCountry, setSortByCountry] = useState(false);
-  const originalUsers = useRef<User[]>([]);
-
+	const originalUsers = useRef<User[]>([]);
+	const [filterCountry, setFilterCountry] = useState<string | null>(null);
 
 	const toggleColors = () => {
 		setShowColors(!showColors);
@@ -18,27 +18,31 @@ function App() {
 	};
 	const handleDelete = (uuid: string) => {
 		const filteredUsers = users.filter((user) => user.login.uuid !== uuid);
-    setUsers(filteredUsers);
+		setUsers(filteredUsers);
 	};
-  const handleReset = () => {
-    setUsers(originalUsers.current);
-  }
+	const handleReset = () => {
+		setUsers(originalUsers.current);
+	};
 
 	useEffect(() => {
 		fetch("https://randomuser.me/api?results=100")
 			.then(async (response) => await response.json())
 			.then((data) => {
-        setUsers(data.results);
-        originalUsers.current = data.results;
-      })
+				setUsers(data.results);
+				originalUsers.current = data.results;
+			})
 			.catch((error) => console.error(error));
 	}, []);
 
+  const filteredUsers = typeof filterCountry === 'string' && filterCountry.length > 0
+  ? users.filter((user) => user.location.country.toLowerCase().includes(filterCountry.toLowerCase()))
+  : users;
+
 	const sortedUsersByCountry = sortByCountry
-		? users.toSorted((a, b) => {
+		? filteredUsers.toSorted((a, b) => {
 				return a.location.country.localeCompare(b.location.country);
 		  })
-		: users;
+		: filteredUsers;
 
 	return (
 		<>
@@ -46,13 +50,18 @@ function App() {
 			<header>
 				<button onClick={toggleColors}>Color cells</button>
 				<button onClick={toggleSortByCountry}>Sort by country</button>
-        <button onClick={handleReset}>Reset Users list</button>
+				<button onClick={handleReset}>Reset Users list</button>
+				<input
+					type="text"
+					placeholder="Filter by country..."
+					onChange={(e) => setFilterCountry(e.target.value)}
+				/>
 			</header>
 			<main>
 				<UserList
 					showColors={showColors}
 					users={sortedUsersByCountry}
-          handleDelete={handleDelete}
+					handleDelete={handleDelete}
 				/>
 			</main>
 		</>
