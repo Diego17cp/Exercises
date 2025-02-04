2a9d2251cf602a078158ceba7d0c65e2c004bc62
Diego17 <diegoalbertocastropastor@gmail.com>
2025-02-04 15:32:09 -0500

    feat: add sorting functionality by country and toggle button in App component

--- Libraries-Frameworks/React/11-technical-interview-list/src/App.tsx
+++ Libraries-Frameworks/React/11-technical-interview-list/src/App.tsx
@@ -1,39 +1,45 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { type User } from './types'
-import { UserList } from './components/UserList'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { type User } from "./types";
+import { UserList } from "./components/UserList";
 
 function App() {
+	const [users, setUsers] = useState<User[]>([]);
+	const [showColors, setShowColors] = useState(false);
+	const [sortByCountry, setSortByCountry] = useState(false);
 
-  const [users, setUsers] = useState<User[]>([])
-  const [showColors, setShowColors] = useState(false)
+	const toggleColors = () => {
+		setShowColors(!showColors);
+	};
+	const toggleSortByCountry = () => {
+		setSortByCountry(!sortByCountry);
+	};
 
-  const toggleColors = () => {
-    setShowColors(!showColors)
-  }
+	useEffect(() => {
+		fetch("https://randomuser.me/api?results=100")
+			.then(async (response) => await response.json())
+			.then((data) => setUsers(data.results))
+			.catch((error) => console.error(error));
+	}, []);
 
-  useEffect(
-    () => {
-      fetch('https://randomuser.me/api?results=100')
-        .then((async response => await response.json()))
-        .then((data) => setUsers(data.results))
-        .catch((error) => console.error(error))
-    }
-    ,[])
+	const sortedUsersByCountry = sortByCountry
+		? users.toSorted((a, b) => {
+				return a.location.country.localeCompare(b.location.country);
+		  })
+		: users;
 
-  return (
-    <>
-      <h1>Hola ts</h1>
-      <header>
-        <button onClick={toggleColors}>
-          Color cells
-        </button>
-      </header>
-      <main>
-        < UserList showColors={showColors} users={users}/>
-      </main>
-    </>
-  )
+	return (
+		<>
+			<h1>Hola ts</h1>
+			<header>
+				<button onClick={toggleColors}>Color cells</button>
+				<button onClick={toggleSortByCountry}>Sort by country</button>
+			</header>
+			<main>
+				<UserList showColors={showColors} users={sortedUsersByCountry} />
+			</main>
+		</>
+	);
 }
 
-export default App
+export default App;
