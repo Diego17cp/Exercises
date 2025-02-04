d5cea7dfd5a83faa86758bc8e2456bde88f1ce40
Diego17 <diegoalbertocastropastor@gmail.com>
2025-02-04 14:44:00 -0500

    feat: fetch and store random user data in state with TypeScript types

--- Libraries-Frameworks/React/11-technical-interview-list/src/App.tsx
+++ Libraries-Frameworks/React/11-technical-interview-list/src/App.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { type User } from './types'
 
 function App() {
+
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(
+    () => {
+      fetch('https://randomuser.me/api?results=100')
+        .then((async response => await response.json()))
+        .then((data) => setUsers(data.results))
+        .catch((error) => console.error(error))
+    }
+    ,[])
 
   return (
     <>
       <h1>Hola ts</h1>
+      
     </>
   )
 }
