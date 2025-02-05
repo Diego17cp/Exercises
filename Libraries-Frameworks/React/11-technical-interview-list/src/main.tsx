0054b2d2845345b9f4f58c689fc01fd6a8cd1621
Diego17 <diegoalbertocastropastor@gmail.com>
2025-02-05 13:19:17 -0500

    feat: install and config for tanstack query use

--- Libraries-Frameworks/React/11-technical-interview-list/src/main.tsx
+++ Libraries-Frameworks/React/11-technical-interview-list/src/main.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+const client = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <QueryClientProvider client={client}>
+        <App />
+    </QueryClientProvider>
 )
