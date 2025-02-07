4cccdc1f33b0e0e789d4d34696aa72cc7df9eafd
Diego17 <diegoalbertocastropastor@gmail.com>
2025-02-07 15:31:40 -0500

    feat: enhance App component layout with Material-UI and JsLogo

--- Libraries-Frameworks/React/12-js-quiz/src/App.tsx
+++ Libraries-Frameworks/React/12-js-quiz/src/App.tsx
@@ -1,11 +1,25 @@
-import './App.css'
+import { Container, Stack, Typography } from "@mui/material";
+import "./App.css";
+import { JsLogo } from "./components/JsLogo";
 
 function App() {
-  return (
-    <>
-      <h1>Hello project</h1>
-    </>
-  )
+	return (
+		<main>
+			<Container maxWidth="sm">
+				<Stack
+					direction={"row"}
+					spacing={2}
+					justifyContent={"center"}
+					alignItems={"center"}
+				>
+					<JsLogo />
+					<Typography variant="h2" align="center" component={"h1"}>
+						JavaScript Quiz
+					</Typography>
+				</Stack>
+			</Container>
+		</main>
+	);
 }
 
-export default App
+export default App;
