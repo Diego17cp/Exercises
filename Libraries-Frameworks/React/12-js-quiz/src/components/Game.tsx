bbd8675db005fa123638f22d5d981cd58597a549
Diego17 <diegoalbertocastropastor@gmail.com>
2025-02-07 16:37:51 -0500

    feat: integrate dark theme and add syntax highlighting to quiz questions

--- Libraries-Frameworks/React/12-js-quiz/src/components/Game.tsx
+++ Libraries-Frameworks/React/12-js-quiz/src/components/Game.tsx
@@ -1,11 +1,37 @@
-import { Card, IconButton, Stack, Typography } from "@mui/material";
+import {
+	Card,
+	IconButton,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemText,
+	Stack,
+	Typography,
+} from "@mui/material";
 import { useQuestionsStore } from "../store/questions";
+import SyntaxHighLighter from "react-syntax-highlighter";
+import { gradientDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { type Question as QuestionType } from "../types.d";
 
 const Question = ({ info }: { info: QuestionType }) => {
 	return (
-		<Card variant="outlined">
+		<Card
+			variant="outlined"
+			sx={{ textAlign: "left", p: 2, bgcolor: "background.paper", mt: 4 }}
+		>
 			<Typography variant="h5">{info.question}</Typography>
+			<SyntaxHighLighter language="javascript" style={gradientDark}>
+				{info.code}
+			</SyntaxHighLighter>
+			<List sx={{ bgcolor: "#333" }} disablePadding>
+				{info.answers.map((answer, index) => (
+					<ListItem key={index} divider>
+						<ListItemButton>
+							<ListItemText primary={answer}></ListItemText>
+						</ListItemButton>
+					</ListItem>
+				))}
+			</List>
 		</Card>
 	);
 };
