10703e559fd075d73fc2d3ad6743868e9d0248ae
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-12 20:31:50 -0500

    feat: refactor directory listing to use async/await and include file details

--- Node/first_app.js
+++ Node/first_app.js
@@ -1,11 +1,35 @@
-const fs = require("node:fs");
+const fs = require("node:fs/promises");
+const path = require("node:path");
+const folder = process.argv[2] ?? ".";
 
-fs.readdir(".", (err, files) => {
-	if (err) {
-		console.error(`Error al leer el directorio: ${err}`);
-		return;
+const ls = async (folder) => {
+	let files;
+	try {
+		files = await fs.readdir(folder);
+	} catch (error) {
+		console.log(`No se pudo leer el directorio ${folder}`);
+		process.exit(1);
 	}
-	files.forEach(
-        (file) => console.log(file)
+
+    const filesPromises = files.map(
+        async (file) => {
+            const filePath = path.join(folder, file);
+            let fileStats
+            try{
+                fileStats = await fs.stat(filePath);
+            } catch (error) {
+                console.log(`No se pudo leer el archivo ${filePath}`);
+                return;
+            }
+            const isDirectory = fileStats.isDirectory();
+            const fileType = isDirectory ? "d" : "-";
+            const fileSize = fileStats.size;
+            const fileModified = fileStats.mtime.toLocaleString();
+
+            return `${fileType} ${file} ${fileSize.toString()} ${fileModified}`;
+        }
     )
-});
+    const filesInfo = await Promise.all(filesPromises);
+    filesInfo.forEach(fileInfo => console.log(fileInfo));
+};
+ls(folder);
