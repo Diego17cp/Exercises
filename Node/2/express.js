b0bfcfabe14de811ebfb42f824bbabae1e0f0b24
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-05 17:28:16 -0500

    feat: enhance POST request handling with manual JSON parsing and error handling

--- Node/2/express.js
+++ Node/2/express.js
@@ -3,17 +3,32 @@ const app = express();
 app.disable('x-powered-by');
 const port = process.env.PORT ?? 3000;
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' });
-})
-app.post('/pokemon', (req, res) => {
+// Forma manual
+app.use((req, res, next) => {
+    if (req.method !== 'POST') return next();
+    if (req.headers['content-type'] !== 'application/json') return next()
     let body = '';
     req.on('data', chunk => {
         body += chunk.toString(); // convert Buffer to string
     });
     req.on('end', () => {
-        const data = JSON.parse(body);
-        res.json({ message: 'Pokemon received!', data });
+        try {
+            req.body = JSON.parse(body);
+            next();
+        } catch (error) {
+            res.status(400).json({ message: 'Invalid JSON' });
+        }
+    });
+})
+// Forma automatica con express
+app.use(express.json())
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World!' });
+})
+app.post('/pokemon', (req, res) => {
+    res.status(201).json({
+        message: 'Pokemon created',
+        pokemon: req.body
     });
 })
 app.use((req, res) => {
