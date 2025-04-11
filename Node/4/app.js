7c9a0159f83dde377043f71484ac47dcd3aa34c2
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-11 16:59:02 -0500

    feat: implement CORS middleware for handling cross-origin requests

--- Node/4/app.js
+++ Node/4/app.js
@@ -1,42 +1,20 @@
 import express, { json } from 'express';
 const app = express();
 import { moviesRouter } from './routes/movies.js';
+import { corsMiddleware } from './middlewares/cors.js';
 
 
 app.disable('x-powered-by');
 
 // Middleware para manejar CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(204);
-    }
-    next();
+    corsMiddleware(req, res, next);
 });
 
 app.use(json());
 
 // Rutas existentes...
-app.get('/movies', (req, res) => {a});
-
-app.get('/movies/:id', (req, res) => {
-    a
-});
-
-app.post('/movies', (req, res) => {
-    a
-});
-
-app.delete('/movies/:id', (req, res) => {
-    a;
-});
-
-app.patch('/movies/:id', (req, res) => {
-    a
-});
-
+app.use('/movies', moviesRouter);
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
