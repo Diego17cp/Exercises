649b8e686f2a1ac28bf81a283bc8be701edbf8e1
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-12 14:43:27 -0500

    feat: implement SQLite database for chat messages and enhance message handling

--- Node/RealTimeChat/server/index.js
+++ Node/RealTimeChat/server/index.js
@@ -2,8 +2,28 @@ import express from "express";
 import logger from "morgan";
 import { Server } from "socket.io";
 import { createServer } from "node:http";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
 
 const port = process.env.PORT ?? 3000;
+
+const initDB = async () => {
+    return open({
+        filename: './db/chat.db',
+        driver: sqlite3.Database
+    }).then(async (db) =>{
+        await db.exec(`
+            CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                content TEXT NOT NULL,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+            );
+        `)
+        return db
+    })
+}
+
+const dbPromise = initDB()
 
 const app = express();
 const server = createServer(app);
@@ -11,14 +31,20 @@ const io = new Server(server, {
     connectionStateRecovery: {}
 });
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
+    const db = await dbPromise
 	console.log("A user connected");
     socket.on('disconnect', () => {
         console.log('A user disconnected');
     })
-    socket.on('chat message', (msg) => {
-        console.log('Message: ' + msg);
-        io.emit('chat message', msg);
+    socket.on('chat message', async (msg) => {
+        try {
+            const result = await db.run(`INSERT INTO messages (content) VALUES (?)`, [msg])
+            io.emit('chat message', msg, result.lastID.toString());
+        } catch (error) {
+            console.error("Error inserting message:", error);
+            return;
+        }
     })
 });
 
