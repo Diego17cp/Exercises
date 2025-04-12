9e62b3fc159d109c1257c6b9e6765581b4c92e4a
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-12 15:19:26 -0500

    feat: enhance chat message handling with username support and message retrieval

--- Node/RealTimeChat/server/index.js
+++ Node/RealTimeChat/server/index.js
@@ -8,44 +8,68 @@ import { open } from "sqlite";
 const port = process.env.PORT ?? 3000;
 
 const initDB = async () => {
-    return open({
-        filename: './db/chat.db',
-        driver: sqlite3.Database
-    }).then(async (db) =>{
-        await db.exec(`
+	return open({
+		filename: "./db/chat.db",
+		driver: sqlite3.Database,
+	}).then(async (db) => {
+		await db.exec(`
             CREATE TABLE IF NOT EXISTS messages (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 content TEXT NOT NULL,
-                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                user TEXT DEFAULT 'Anonymous'
             );
-        `)
-        return db
-    })
-}
+        `);
+		return db;
+	});
+};
 
-const dbPromise = initDB()
+const dbPromise = initDB();
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-    connectionStateRecovery: {}
+	connectionStateRecovery: {},
 });
 
 io.on("connection", async (socket) => {
-    const db = await dbPromise
+	const db = await dbPromise;
 	console.log("A user connected");
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    })
-    socket.on('chat message', async (msg) => {
-        try {
-            const result = await db.run(`INSERT INTO messages (content) VALUES (?)`, [msg])
-            io.emit('chat message', msg, result.lastID.toString());
-        } catch (error) {
-            console.error("Error inserting message:", error);
-            return;
-        }
-    })
+	socket.on("disconnect", () => {
+		console.log("A user disconnected");
+	});
+	socket.on("chat message", async (msg) => {
+		try {
+			const user = socket.handshake.auth.username ?? "Anonymous";
+			const result = await db.run(
+				`INSERT INTO messages (content, user) VALUES (?, ?)`,
+				[msg, user]
+			);
+			io.emit("chat message", msg, result.lastID.toString(), user);
+		} catch (error) {
+			console.error("Error inserting message:", error);
+			return;
+		}
+	});
+	if (!socket.recovered) {
+		try {
+			const results = await db.all(
+				`SELECT * FROM messages WHERE id > (?)`,
+				[socket.handshake.auth.serverOffset ?? 0]
+			);
+			results.forEach((row) => {
+				socket.emit(
+					"chat message",
+					row.content,
+					row.id.toString(),
+					row.user
+				);
+			});
+		} catch (error) {
+			console.error("Error retrieving messages:", error);
+			return;
+		}
+	}
 });
 
 app.use(logger("dev"));
