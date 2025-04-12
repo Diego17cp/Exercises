4a5f8ea4fbf8591de2abbad919a5ef9602643b4d
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-11 19:48:49 -0500

    fix: remove unused import statements in MovieController

--- Node/4/controllers/movies.js
+++ Node/4/controllers/movies.js
@@ -2,12 +2,13 @@ import { MovieModel } from "../models/movie.js";
 import { validateMovie, validatePartialMovie } from "../schemes/movies.js";
 
 export class MovieController {
-    static getAll = async ({ req, res }) => {
+    static getAll = async (req, res) => {
         const { genre } = req.query;
         const movies = await MovieModel.getAll({ genre });
         res.json(movies);
     }
-    static getById = async ({ req, res }) => {
+
+    static getById = async (req, res) => {
         const { id } = req.params;
         try {
             const movie = await MovieModel.getById({ id });
@@ -16,19 +17,22 @@ export class MovieController {
             res.status(404).json({ error: error.message });
         }
     }
-    static create = async ({ req, res }) => {
+
+    static create = async (req, res) => {
         const result = validateMovie(req.body);
         if (result.error) return res.status(400).json({ error: JSON.parse(result.error.message) });
         const newMovie = await MovieModel.create({ input: result.data });
         res.status(201).json(newMovie);
     }
-    static delete = async ({ req, res }) => {
+
+    static delete = async (req, res) => {
         const { id } = req.params;
         const result = await MovieModel.delete({ id });
         if (!result) return res.status(404).json({ message: 'Movie not found' });
         return res.json({ message: 'Movie deleted' });
     }
-    static update = async ({ req, res }) => {
+
+    static update = async (req, res) => {
         const { id } = req.params;
         const result = validatePartialMovie(req.body);
         if (result.error) return res.status(400).json({ error: JSON.parse(result.error.message) });
