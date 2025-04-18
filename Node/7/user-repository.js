66e673408e626d13f715a882172cdfc3df88ac7c
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-18 15:53:30 -0500

    refactor: improve validation logic for user creation

--- Node/7/user-repository.js
+++ Node/7/user-repository.js
@@ -1,41 +1,56 @@
-import DBLOCAL from 'db-local'
-import bcrypt from 'bcryptjs'
+import DBLOCAL from "db-local";
+import bcrypt from "bcryptjs";
 
-const { Schema } = new DBLOCAL({ path: './db' })
+const { Schema } = new DBLOCAL({ path: "./db" });
 
-const User = Schema('User', {
-    _id: { type: String, required: true },
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-})
+const User = Schema("User", {
+	_id: { type: String, required: true },
+	username: { type: String, required: true },
+	password: { type: String, required: true },
+});
 
 export class UserRepository {
-    static async create ({ username, password }) {
-        // Min Validations
-        if (typeof username !== 'string') throw new Error('username must be a string')
-        if (username.length < 3) throw new Error('username must be at least 3 characters long')
-        
-        if (typeof password !== 'string') throw new Error('password must be a string')
-        if (password.length < 8) throw new Error('password must be at least 8 characters long')
+	static async create({ username, password }) {
+		// Min Validations
 
-        // Check if user already exists
-        const user = User.findOne({ username })
-        if (user) throw new Error('username already exists')
+		Validation.username(username);
+		Validation.password(password);
 
-        // Create id
-        const id = crypto.randomUUID()
+		// Check if user already exists
+		const user = User.findOne({ username });
+		if (user) throw new Error("username already exists");
 
-        // Hash password
-        const hashedPassword = await bcrypt.hash(password, 10)
+		// Create id
+		const id = crypto.randomUUID();
 
-        // Create user
-        User.create({
-            _id: id,
-            username,
-            password: hashedPassword,
-        }).save()
+		// Hash password
+		const hashedPassword = await bcrypt.hash(password, 10);
 
-        return id
-    }
-    static login ({ username, password }) {}
+		// Create user
+		User.create({
+			_id: id,
+			username,
+			password: hashedPassword,
+		}).save();
+
+		return id;
+	}
+	static login({ username, password }) {
+		Validation.username(username);
+		Validation.password(password);
+	}
+}
+class Validation {
+	static username(username) {
+		if (typeof username !== "string")
+			throw new Error("username must be a string");
+		if (username.length < 3)
+			throw new Error("username must be at least 3 characters long");
+	}
+	static password(password) {
+		if (typeof password !== "string")
+			throw new Error("password must be a string");
+		if (password.length < 8)
+			throw new Error("password must be at least 8 characters long");
+	}
 }
