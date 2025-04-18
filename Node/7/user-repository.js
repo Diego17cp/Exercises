a795da18b3d68b3755cca1140713af0b173d1cab
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-18 16:00:58 -0500

    feat: make login method asynchronous and update password comparison logic

--- Node/7/user-repository.js
+++ Node/7/user-repository.js
@@ -35,15 +35,15 @@ export class UserRepository {
 
 		return id;
 	}
-	static login({ username, password }) {
+	static async login({ username, password }) {
 		Validation.username(username);
 		Validation.password(password);
 
         const user = User.findOne({ username });
         if (!user) throw new Error("username not found");
-        const isValid = bcrypt.compareSync(password, user.password);
+        const isValid = await bcrypt.compare(password, user.password);
 		if (!isValid) throw new Error("invalid password");
-		
+
 		return user
 	}
 }
