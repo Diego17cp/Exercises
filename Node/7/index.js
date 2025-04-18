1c6f13d9e48fd09e6443293cce2f09e58e331a71
Diego17 <diegoalbertocastropastor@gmail.com>
2025-04-18 15:31:28 -0500

    feat: implement user registration functionality and user repository with validations

--- Node/7/index.js
+++ Node/7/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { PORT } from "./config.js";
+import { UserRepository } from "./user-repository.js";
 
 const app = express();
 
@@ -7,7 +8,15 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 })
 app.post('/login', (req, res) => {})
-app.post('/register', (req, res) => {})
+app.post('/register', (req, res) => {
+    const { username, password } = req.body
+    try {
+        const id = UserRepository.create({ username, password })
+        res.status(201).json({ id })
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
 app.post('/logout', (req, res) => {})
 app.get('/protected', (req, res) => {})
 
