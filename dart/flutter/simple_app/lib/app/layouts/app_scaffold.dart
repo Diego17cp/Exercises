5d3dcd8111436fa8d593cfc79933ee5d3a2145b5
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-29 12:07:05 -0500

    feat: Refactor AppScaffold to use child widget and integrate bottom navigation

--- dart/flutter/simple_app/lib/app/layouts/app_scaffold.dart
+++ dart/flutter/simple_app/lib/app/layouts/app_scaffold.dart
@@ -1,37 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:simple_app/app/layouts/widgets/bottom_nav.dart';
+import 'package:simple_app/app/routes/routes.dart';
 
 class AppScaffold extends StatelessWidget {
-  final String title;
-  final Widget body;
-  final bool showBottomNavBar;
+  final Widget child;
 
   const AppScaffold({
     super.key,
-    required this.title,
-    required this.body,
-    this.showBottomNavBar = true,
+    required this.child,
   });
 
   @override
   Widget build(BuildContext context) {
+    final String location = GoRouterState.of(context).uri.toString();
+    
+    int currentIndex = tabs.indexWhere((tab) {
+      if (tab.path == "/") return location == "/";
+      return location.startsWith(tab.path);
+    });
+    
+    if (currentIndex == -1) currentIndex = 0;
+    
+    void onNavigationTap(int index) {
+      if (index != currentIndex) GoRouter.of(context).push(tabs[index].path);
+    }
+    
+    final currentTab = tabs[currentIndex];
+
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: body,
-      bottomNavigationBar: showBottomNavBar
-      // This is just a placeholder BottomNavigationBar, must be in another file
-          ? BottomNavigationBar(
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.settings),
-                  label: 'Settings',
-                ),
-              ],
-            )
-          : null,
+      appBar: AppBar(title: Text(currentTab.title)),
+      body: child,
+      bottomNavigationBar: BottomNav(
+        currentIndex: currentIndex,
+        onTap: onNavigationTap,
+      ),
     );
   }
 }
