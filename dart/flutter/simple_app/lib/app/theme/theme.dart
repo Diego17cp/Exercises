8fe496a92f5a62dcf7feabb2a752a19a67467ecd
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-29 14:29:48 -0500

    feat: Update colorSchemeSeed for light and dark themes to match design specifications

--- dart/flutter/simple_app/lib/app/theme/theme.dart
+++ dart/flutter/simple_app/lib/app/theme/theme.dart
@@ -4,11 +4,11 @@ class AppTheme {
   static ThemeData light = ThemeData(
     brightness: Brightness.light,
     useMaterial3: true,
-    colorSchemeSeed: Color(0xFF0E669D)
+    colorSchemeSeed: Color(0xFF1A9A5E)
   );
   static ThemeData dark = ThemeData(
     brightness: Brightness.dark,
     useMaterial3: true,
-    colorSchemeSeed: Color(0xFF0E669D)
+    colorSchemeSeed: Color(0xFF1A9A5E)
   );
 }
