8195b4b12ffa277cf11fca5241d1ad93e7fab117
Diego17 <diegoalbertocastropastor@gmail.com>
2025-12-29 12:06:52 -0500

    feat: Refactor router configuration to use ShellRoute for improved navigation structure

--- dart/flutter/simple_app/lib/app/routes/router.dart
+++ dart/flutter/simple_app/lib/app/routes/router.dart
@@ -2,56 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:simple_app/app/layouts/app_scaffold.dart';
 
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'shell');
+
 final GoRouter router = GoRouter(
+  navigatorKey: _rootNavigatorKey,
+  initialLocation: '/',
   routes: [
-    GoRoute(
-      path: "/",
-      builder: (context, state) => AppScaffold(
-        title: "Home",
-        body: Container(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Welcome to the Home Page"),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  GoRouter.of(context).push('/settings');
-                },
-                child: Text("Go to Settings"),
+    ShellRoute(
+      navigatorKey: _shellNavigatorKey,
+      builder: (context, state, child) => AppScaffold(child: child),
+      routes: [
+        GoRoute(
+          path: '/',
+          pageBuilder: (context, state) => NoTransitionPage(
+            child: Container(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Welcome to the Home Page"),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      GoRouter.of(context).push('/login');
+                    },
+                    child: Text("Go to Login"),
+                  ),
+                ],
               ),
-              SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  GoRouter.of(context).push('/login');
-                },
-                child: Text("Go to Login"),
-              ),
-            ],
+            ),
           ),
         ),
-      ),
+        GoRoute(
+          path: '/settings',
+          pageBuilder: (context, state) => NoTransitionPage(
+            child: Container(
+              alignment: Alignment.center,
+              child: Text("Welcome to the Settings Page"),
+            ),
+          ),
+        ),
+      ],
     ),
     GoRoute(
-      path: "/settings",
-      builder: (context, state) => AppScaffold(
-        title: "Settings",
-        body: Container(
-          alignment: Alignment.center,
-          child: Text("Welcome to the Settings Page"),
+      parentNavigatorKey: _rootNavigatorKey,
+      path: '/login',
+      builder: (context, state) => Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Welcome to the Login Page"),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => GoRouter.of(context).pop(),
+              child: Text("Go Back"),
+            ),
+          ],
         ),
-      ),
-    ),
-    GoRoute(
-      path: "/login",
-      builder: (context, state) => AppScaffold(
-        title: "Login",
-        body: Container(
-          alignment: Alignment.center,
-          child: Text("Welcome to the Login Page"),
-        ),
-        showBottomNavBar: false,
       ),
     ),
   ],
